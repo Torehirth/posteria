b469723c6ee4866832fea0ba8cac70a1b42de53a
Tore Mæland Hirth <torehirth@gmail.com>
2024-11-15 19:43:36 +0100

    add html and style to form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.{html,js}", "!./node_modules/**/*.{html,js}"],
-  darkMode: "class",
+  darkMode: "selector",
   theme: {
     extend: {
       colors: {
         darkGray: "#323436",
         black: "#181A1C",
         white: "#F9F9F9",
+        eden: {
+          50: "#F0FDF9",
+          100: "#CBFCEE",
+          200: "#97F8DC",
+          300: "#5BEDC9",
+          400: "#2AD7B2",
+          500: "#11BB99",
+          600: "#0A977D",
+          700: "#0D7866",
+          800: "#106356",
+          900: "#124F45",
+          950: "#03302B",
+        },
       },
     },
   },
