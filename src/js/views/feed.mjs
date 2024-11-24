22b7717cdfe24541b05e5e23f2d76385adb6cff0
Tore Mæland Hirth <torehirth@gmail.com>
2024-11-24 13:14:09 +0100

    add search bar to larger screens, add sort menu on feed page

--- src/js/views/feed.mjs
+++ src/js/views/feed.mjs
@@ -1,9 +1,28 @@
 // Theme mode
 import { applySystemTheme, toggleColourTheme } from "../handlers/themeHandlers.mjs";
+import {
+  toggleAsideSearchbar,
+  closeSearchBarOnKeypress,
+  closeSearchbarOnClick,
+} from "../handlers/searchbarHandlers.mjs";
+import {
+  closeSortDropDownByClickOutside,
+  closeSortDropdownByEscKey,
+  toggleSortDropdownMenu,
+} from "../handlers/sortDropdownMenuHandlers.mjs";
+
+// -- Theme --
 const themeToggleBtn = document.querySelector("#theme-toggle-btn");
 const headerThemeToggleBtn = document.querySelector("#header-theme-toggle-btn");
-
 themeToggleBtn.addEventListener("click", toggleColourTheme);
 headerThemeToggleBtn.addEventListener("click", toggleColourTheme);
-
 applySystemTheme();
+// -- Searchbar --
+toggleAsideSearchbar();
+closeSearchBarOnKeypress();
+closeSearchbarOnClick();
+
+// -- Sort dropdown menu --
+toggleSortDropdownMenu();
+closeSortDropDownByClickOutside();
+closeSortDropdownByEscKey();
