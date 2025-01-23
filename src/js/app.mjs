f8cbeca2634d3197d70141abe381f6816c777cae
Tore Mæland Hirth <torehirth@gmail.com>
2025-01-23 11:06:36 +0100

    moved all the js code from specific views files to the router function

--- src/js/app.mjs
+++ src/js/app.mjs
@@ -1,20 +1,69 @@
+import { applySystemTheme, toggleColourTheme } from "./handlers/themeHandlers.mjs";
+import {
+  displayFollowersSection,
+  displayPostsSection,
+  displayFollowingSection,
+} from "./handlers/profileTabHandlers.mjs";
+import {
+  toggleAsideSearchbar,
+  closeSearchBarOnKeypress,
+  closeSearchbarOnClick,
+} from "./handlers/searchbarHandlers.mjs";
+import {
+  closeSortDropDownByClickOutside,
+  closeSortDropdownByEscKey,
+  toggleSortDropdownMenu,
+} from "./handlers/sortDropdownMenuHandlers.mjs";
+
 const router = () => {
   const pathname = window.location.pathname;
-
-  console.log(pathname);
+  const headerThemeToggleBtn = document.querySelector("#header-theme-toggle-btn");
+  const themeToggleBtn = document.querySelector("#theme-toggle-btn");
 
   switch (pathname) {
-    case "./":
-      console.log("#HOME");
+    case "/":
+    case "/index.html":
+      console.log("#LOG IN PAGE");
+      // -- Theme --
+      headerThemeToggleBtn.addEventListener("click", toggleColourTheme);
+      applySystemTheme();
       break;
-    case "./register":
-      console.log("REGISTER");
+
+    case "/register":
+    case "/register/index.html":
+      console.log("REGISTER PAGE");
+      // -- Theme --
+      headerThemeToggleBtn.addEventListener("click", toggleColourTheme);
+      applySystemTheme();
       break;
-    case "./feed":
-      console.log("FEED");
+
+    case "/feed":
+    case "/feed/index.html":
+      console.log("FEED PAGE");
+      // -- Theme --
+      themeToggleBtn.addEventListener("click", toggleColourTheme);
+      headerThemeToggleBtn.addEventListener("click", toggleColourTheme);
+      applySystemTheme();
+      // -- Searchbar --
+      toggleAsideSearchbar();
+      closeSearchBarOnKeypress();
+      closeSearchbarOnClick();
+      // -- Sort dropdown menu --
+      toggleSortDropdownMenu();
+      closeSortDropDownByClickOutside();
+      closeSortDropdownByEscKey();
       break;
-    case "./profile":
-      console.log("PROFILE");
+
+    case "/profile":
+    case "/profile/index.html":
+      console.log("PROFILE PAGE");
+      // -- Posts/followers/follow section --
+      displayPostsSection();
+      displayFollowersSection();
+      displayFollowingSection();
+      // -- Theme --
+      themeToggleBtn.addEventListener("click", toggleColourTheme);
+      applySystemTheme();
       break;
   }
 };
