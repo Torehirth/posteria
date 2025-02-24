a445bbbb189a725b78c0085dfc12c521f1210913
Tore Mæland Hirth <torehirth@gmail.com>
2025-02-24 15:17:13 +0100

    updated the error handling on numberOfPosts function

--- src/js/ui/common/handlers/updateNumberPostsProfile.mjs
+++ src/js/ui/common/handlers/updateNumberPostsProfile.mjs
@@ -3,7 +3,7 @@ const numberOfPostsWrapper = document.querySelector("#number-posts");
 export const updateNumberOfPosts = (postsData) => {
   const numberOfPosts = postsData.length || "?";
 
-  if (numberOfPosts !== typeof number) {
+  if (typeof numberOfPosts !== "number") {
     console.error("Number of posts is not a number");
   }
   numberOfPostsWrapper.textContent = numberOfPosts;
